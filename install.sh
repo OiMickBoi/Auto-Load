a3c846afa371ade04a4cdf9646be1c2b145c2be4
oimickboi <oimickboi@gmail.com>
2023-12-25 00:48:49 -0700

    fixed some errors

--- install.sh
+++ install.sh
@@ -6,30 +6,29 @@ sh <(curl -L https://nixos.org/nix/install) --daemon
 
 # install packages
 nix-env -iA \
-	nixpkgs.zsh \ 
 	nixpkgs.neovim \
 	nixpkgs.tmux \
-	nixpkgs.stow 
-#	nixpkgs.fish \ 
+	nixpkgs.stow \
+	nixpkgs.zsh \
 
 # stow dotfiles
-stow git
 stow nvim
 stow tmux
-stow zsh
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+command -v zsh | sudo tee -a /etc/shells
 # use zsh as default shell
 sudo chsh -s $(which zsh) $USER
-#sudo chsh -s $(which fish) $USER
-
        
 # Install oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
+rm ~/.zshrc
+stow zsh
+
 # Clone necessary plugins.
-git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
